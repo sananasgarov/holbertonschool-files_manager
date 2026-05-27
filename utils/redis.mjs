208bb3bb4e75f9739eb62d79ef5af70b93471da0
you@example.com <prog58@gmail.com>
2026-05-27 12:59:18 +0400

    dsa

--- utils/redis.mjs
+++ utils/redis.mjs
@@ -1,3 +1,4 @@
+import { promisify } from 'util';
 import { createClient } from 'redis';
 
 class RedisClient {
@@ -6,24 +7,52 @@ class RedisClient {
     this.client.on('error', (error) => {
       console.log(`Redis client error: ${error}`);
     });
-    this.client.connect();
+
+    if (typeof this.client.connect === 'function') {
+      this.client.connect().catch((error) => {
+        console.log(`Redis client error: ${error}`);
+      });
+    } else {
+      this.getAsync = promisify(this.client.get).bind(this.client);
+      this.setAsync = promisify(this.client.set).bind(this.client);
+      this.delAsync = promisify(this.client.del).bind(this.client);
+      this.setexAsync = promisify(this.client.setex).bind(this.client);
+    }
   }
 
   isAlive() {
-    return this.client.isOpen;
+    if (typeof this.client.isOpen === 'boolean') {
+      return this.client.isOpen;
+    }
+
+    return this.client.connected;
   }
 
   async get(key) {
+    if (this.getAsync) {
+      return this.getAsync(key);
+    }
+
     return this.client.get(key);
   }
 
   async set(key, value, duration) {
-    await this.client.set(key, value, {
+    if (this.setexAsync) {
+      await this.setexAsync(key, duration, value);
+      return;
+    }
+
+    await this.setAsync(key, value, {
       EX: duration,
     });
   }
 
   async del(key) {
+    if (this.delAsync) {
+      await this.delAsync(key);
+      return;
+    }
+
     await this.client.del(key);
   }
 }
